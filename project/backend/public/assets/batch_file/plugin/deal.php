1702826ed1a2289f13d43b452e4f7442d57839c7
hahaha0417 <hahaha0417@gmail.com>
2020-10-24 13:00:12 +0800

    plugin 腳本製作，避免github自動檢查

--- project/backend/public/assets/batch_file/plugin/deal.php
+++ project/backend/public/assets/batch_file/plugin/deal.php
@@ -14,7 +14,7 @@ use hahahalib\script\hahaha_file_deal_define_key as key;
 use hahahalib\script\hahaha_file_deal_define_operator as op;
 
 $src_ = realpath(dirname(__FILE__) . "/node_modules");
-$dst_ = realpath($root_ . "/public/assets");
+$dst_ = realpath($root_ . "/public/assets/plugin");
 
 // echo $src_."\n".$dst_;
 // return;
@@ -28,43 +28,154 @@ $list_ = [
 	// -------------------------------------------- 
 	"group1" => [
 		// -------------------------------------------- 
-		"item1" => [
-			// -------------------------------------------- 
-			// 刪除plugin
-			// -------------------------------------------- 
+		
+		// -------------------------------------------- 
+		// 刪除plugin
+		// -------------------------------------------- 
+		"item1" => [			
 			op::DELETE_TREE => [
 				key::DIR => $dst_ . "/" . "plugin",
 			],
 		],
-		"item2" => [
-			// -------------------------------------------- 
-			// 新增plugin
-			// -------------------------------------------- 
+		// -------------------------------------------- 
+		// 新增plugin
+		// -------------------------------------------- 
+		"item2" => [			
 			op::MKDIR => [
 				key::DIR => $dst_ . "/" . "plugin",
 				key::MODE => 0775,
 			],
 		],
-		"item3" => [
-			// -------------------------------------------- 
-			// 處理插件位置
-			// -------------------------------------------- 
+		// -------------------------------------------- 
+		// 處理插件位置
+		// -------------------------------------------- 
+		
+		// ---------------------------------- 
+		// base
+		// ---------------------------------- 
+		"jquery" => [			
 			op::MOVE => [
 				key::SRC => $src_ . "/" . "jquery",
 				key::DST => $dst_ . "/plugin/" . "jquery",
 			],
 		],
-		"item4" => [
-			// -------------------------------------------- 
-			// 刪除node_modules
-			// -------------------------------------------- 
+		"bootstrap" => [			
+			op::MOVE => [
+				key::SRC => $src_ . "/" . "bootstrap",
+				key::DST => $dst_ . "/plugin/" . "bootstrap",
+			],
+		],
+		// ---------------------------------- 
+		// jquery plugin
+		// ---------------------------------- 
+		"mkdir jquery_plugin" => [			
+			op::MKDIR => [
+				key::DIR => $dst_ . "/plugin/" . "jquery_plugin",
+				key::MODE => 0775,
+			],
+		],
+		"jquery-autocomplete" => [			
+			op::MOVE => [
+				key::SRC => $src_ . "/" . "jquery-autocomplete",
+				key::DST => $dst_ . "/plugin/" . "jquery_plugin/jquery-autocomplete",
+			],
+		],
+		"jquery-file-upload" => [			
+			op::MOVE => [
+				key::SRC => $src_ . "/" . "jquery-file-upload",
+				key::DST => $dst_ . "/plugin/" . "jquery_plugin/jquery-file-upload",
+			],
+		],
+		// ---------------------------------- 
+		// ckeditor
+		// ---------------------------------- 		
+		"mkdir ckeditor" => [			
+			op::MKDIR => [
+				key::DIR => $dst_ . "/plugin/" . "ckeditor",
+				key::MODE => 0775,
+			],
+		],
+		"ckeditor4" => [			
+			op::MOVE => [
+				key::SRC => $src_ . "/" . "ckeditor4",
+				key::DST => $dst_ . "/plugin/" . "ckeditor/ckeditor4",
+			],
+		],
+		"ckeditor5" => [			
+			op::MOVE => [
+				key::SRC => $src_ . "/" . "ckeditor5",
+				key::DST => $dst_ . "/plugin/" . "ckeditor/ckeditor5",
+			],
+		],
+		// ---------------------------------- 
+		// 
+		// ---------------------------------- 
+		"async" => [			
+			op::MOVE => [
+				key::SRC => $src_ . "/" . "async",
+				key::DST => $dst_ . "/plugin/" . "async",
+			],
+		],
+		"form" => [			
+			op::MOVE => [
+				key::SRC => $src_ . "/" . "form",
+				key::DST => $dst_ . "/plugin/" . "form",
+			],
+		],
+		"labelauty" => [			
+			op::MOVE => [
+				key::SRC => $src_ . "/" . "labelauty",
+				key::DST => $dst_ . "/plugin/" . "labelauty",
+			],
+		],
+		"layer" => [			
+			op::MOVE => [
+				key::SRC => $src_ . "/" . "layer",
+				key::DST => $dst_ . "/plugin/" . "layer",
+			],
+		],
+		"lazyload" => [			
+			op::MOVE => [
+				key::SRC => $src_ . "/" . "lazyload",
+				key::DST => $dst_ . "/plugin/" . "lazyload",
+			],
+		],
+		"owl.carousel" => [			
+			op::MOVE => [
+				key::SRC => $src_ . "/" . "owl.carousel",
+				key::DST => $dst_ . "/plugin/" . "owl.carousel",
+			],
+		],
+		"slider-pro" => [			
+			op::MOVE => [
+				key::SRC => $src_ . "/" . "slider-pro",
+				key::DST => $dst_ . "/plugin/" . "slider-pro",
+			],
+		],
+		"slim-scroll" => [			
+			op::MOVE => [
+				key::SRC => $src_ . "/" . "slim-scroll",
+				key::DST => $dst_ . "/plugin/" . "slim-scroll",
+			],
+		],
+		"validator" => [			
+			op::MOVE => [
+				key::SRC => $src_ . "/" . "validator",
+				key::DST => $dst_ . "/plugin/" . "validator",
+			],
+		],
+		// -------------------------------------------- 
+		// 刪除node_modules
+		// -------------------------------------------- 
+		"item4" => [			
 			op::DELETE_TREE => [
 			 	key::DIR => $src_ ,
-			],
-			// -------------------------------------------- 
-			// 
-			// -------------------------------------------- 
+			],			
 		],
+		// -------------------------------------------- 
+		// 
+		// -------------------------------------------- 
+		
 		// -------------------------------------------- 
 	],
 	// -------------------------------------------- 
---
+++ project/backend/public/assets/batch_file/plugin/npm install plugin.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/sh
+
+npm init -y
+
+npm i jquery
+npm i bootstrap
+npm i jquery-file-upload
+npm i labelauty
+npm i lazyload
+npm i jquery-autocomplete
+npm i layer
+npm i ckeditor4
+# ckeditor5 還在開發中，沒有原始碼編輯器
+npm i ckeditor5
+npm i owl.carousel
+npm i slider-pro
+npm i slim-scroll
+
+#npm install
