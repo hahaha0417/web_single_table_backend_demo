56d9fae47f09bdd5dbfd5232169bedc947165aff
hahaha0417 <hahaha0417@gmail.com>
2020-11-05 15:49:01 +0800

    更新

--- libraries/hahahalib/composite/cache/doctrine/hahaha_cache_doctrine.php
+++ libraries/hahahalib/composite/cache/doctrine/hahaha_cache_doctrine.php
@@ -71,6 +71,21 @@ namespace hahahalib\cache;
 /*
 我不是要Wrapper他，只是用來快速初始化
 要wrapper成模組，請另外做，不過我覺得意義不大，它本身就是做好的模組了
+
+ --------------------------------------------- 
+注意
+ --------------------------------------------- 
+因為Cache通常不會用在不同用途，因此"我不需要做多個模組獨立使用"(一種Cache，用一個Class單例)
+所以統一用下面class方式設定(如果要Custom設定參數，"寫固定Script統一初始化"(一個專案通常只需要初始化一種)
+(不是Initail設定，因為這裡是基礎模組，Initial會很奇怪，可能會有組合Initail))
+ --------------------------------------------- 
+library內沒必要幫其他框架做初始化並一直維護
+注意 : 意思是設定為附加，不應該放在library裡面，除非是通用的
+ --------------------------------------------- 
+*/
+
+/*
+Cache 容器
 */
 class hahaha_cache_doctrine
 {
@@ -117,11 +132,11 @@ class hahaha_cache_doctrine
         return $this->Redis_;   
     }
 
-    public function Memcached($memcached)
+    public function Memcached($memcached = NULL)
     {
         if(empty($this->Memcached_))
         {
-            if(empty($redis))
+            if(empty($memcached))
             {
                 $memcached_ = new \Memcached();
                 $memcached_->addServer('memcache_host', 11211);
